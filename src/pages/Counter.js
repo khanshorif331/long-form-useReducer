60fd373966311878d1d2880145d25834f0793e52
khanshorif331 <khanshorif331@gmail.com>
2022-12-26 23:06:28 +0600

    counter app done

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -5,28 +5,32 @@ const Counter = () => {
 	const initialState = 0
 	const reducer = (state, action) => {
 		if (action.type === 'INCREMENT') {
-			return state + 1
+			return state + action.payload.count
 		} else if (action.type === 'DECREMENT') {
-			return state - 1
+			return state - action.payload.count
 		}
 	}
 	const [state, dispatch] = useReducer(reducer, initialState)
 	return (
 		<div className="w-full h-screen flex justify-center items-center">
-			<div className="h-[300px] w-[400px] border border-red-300 p-8 d-flex items-center justify-center">
-				<p className="text-2xl">Count : {state}</p>
-				<div className="mt-10">
+			<div className="h-[300px] w-[400px] border border-red-300 p-8">
+				<p className="text-2xl text-center">Count : {state}</p>
+				<div className="mt-10 text-center">
 					<button
 						className="px-4 py-2 bg-blue-500 text-white mr-4"
-						onClick={() => dispatch({ type: 'DECREMENT' })}
+						onClick={() =>
+							dispatch({ type: 'DECREMENT', payload: { count: 5 } })
+						}
 					>
-						-
+						Decrement
 					</button>
 					<button
 						className="px-4 py-2 bg-blue-500 text-white "
-						onClick={() => dispatch({ type: 'INCREMENT' })}
+						onClick={() =>
+							dispatch({ type: 'INCREMENT', payload: { count: 5 } })
+						}
 					>
-						+
+						Increment
 					</button>
 				</div>
 			</div>
